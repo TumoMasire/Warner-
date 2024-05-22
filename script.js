e55ebe8922aecaedc0bbf2a02715085e1911b0e3
TumoMasire <Cse23-057@thuto.bac.ac.bw>
2024-05-22 12:04:43 +0200

    script.js

--- script.js
+++ script.js
@@ -1,24 +1,10 @@
 // JavaScript
-document.getElementById('feedback-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    var form = this;
-    var name = document.getElementById('name').value;
-    var email = document.getElementById('email').value;
-    var suggestions = document.getElementById('suggestions').value;
-
-    var subject = 'Feedback Form Submission';
-    var body = 'Name: ' + name + '\nEmail: ' + email + '\nSuggestions: ' + suggestions;
-
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', 'https://mail.google.com/mail/feed/atom', true);
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    xhr.send('to=masiretumo11@gmail.com&subject=' + subject + '&body=' + body);
-
-    xhr.onload = function() {
-        if (xhr.status === 200) {
-            alert('Your feedback has been sent successfully!');
-        } else {
-            alert('Error sending feedback. Please try again.');
+function handleSubmit(event) {   
+          event.preventDefault(); // Prevent the default form submission
+          window.location.href = 'sent.html'; // Redirect to sent.html 
         }
-    };
-});
+
+//games and apps play button
+function playGame(url) {
+    window.open(url, '_blank');
+  }
